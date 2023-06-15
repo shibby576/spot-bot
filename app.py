4f4fdb9b073e2b8453e2246b3e22cb3a4bbddddb
Jonathan Hirko <jhirko12@gmail.com>
2023-06-15 09:55:18 -0400

    updated

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import os
 from flask import Flask, redirect, render_template, request, url_for
 
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-@app.route("/", methods=("GET", "POST"))
-def index():
-    
-    return render_template("index.html")
-
+if __name__ == '__main__':
+    app.run(debug=True)
